077408aef25bfd8cff78e61ce92e1138ebd41ee0
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2020-04-03 21:06:08 +0500

    Exclude .min in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ const rename = require('gulp-rename');
 
 
 function minifyCss() {
-  return src(['admin/**/*.css', '!admin/**/*/.min.css'])
+  return src(['admin/**/*.css', '!admin/**/*/*.min.css'])
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest('admin/'));
 }
 
 function minifyJs() {
-  return src(['frontend/**/*.js', '!frontend/**/*/.min.js'])
+  return src(['frontend/**/*.js', '!frontend/**/*/*.min.js'])
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(dest('frontend/'));
