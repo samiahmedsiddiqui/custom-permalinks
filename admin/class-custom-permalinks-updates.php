bb274377cc1c05255779356ce80decf43c271b60
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2020-09-08 14:19:47 +0500

    Changing PHP curl with wp_remote_post

--- admin/class-custom-permalinks-updates.php
+++ admin/class-custom-permalinks-updates.php
@@ -42,38 +42,28 @@ class Custom_Permalinks_Updates {
 	 * @return void
 	 */
 	private function update_version_details() {
-		if ( true === function_exists( 'curl_init' ) ) {
-			$admin_email = get_bloginfo( 'admin_email' );
-			$curl_url    = 'https://www.custompermalinks.com/plugin-update/';
-			$site_name   = get_bloginfo( 'name' );
-			$site_url    = get_bloginfo( 'wpurl' );
-			$wp_version  = get_bloginfo( 'version' );
+		$admin_email = get_bloginfo( 'admin_email' );
+		$request_url = 'https://www.custompermalinks.com/plugin-update/';
+		$site_name   = get_bloginfo( 'name' );
+		$site_url    = get_bloginfo( 'wpurl' );
+		$wp_version  = get_bloginfo( 'version' );
 
-			$updates = array(
-				'action'         => $this->method,
-				'admin_email'    => $admin_email,
-				'plugin_version' => CUSTOM_PERMALINKS_VERSION,
-				'site_name'      => $site_name,
-				'site_url'       => $site_url,
-				'wp_version'     => $wp_version,
-			);
+		$updates = array(
+			'action'         => $this->method,
+			'admin_email'    => $admin_email,
+			'plugin_version' => CUSTOM_PERMALINKS_VERSION,
+			'site_name'      => $site_name,
+			'site_url'       => $site_url,
+			'wp_version'     => $wp_version,
+		);
 
-			// Create a connection.
-			$curl_conn = curl_init( $curl_url );
-
-			// Generate URL-encoded query string.
-			$encoded_data = http_build_query( $updates, '', '&' );
-
-			// Setting options.
-			curl_setopt( $curl_conn, CURLOPT_POST, 1 );
-			curl_setopt( $curl_conn, CURLOPT_POSTFIELDS, $encoded_data );
-			curl_setopt( $curl_conn, CURLOPT_RETURNTRANSFER, true );
-
-			// Execute the given cURL session.
-			curl_exec( $curl_conn );
-
-			// Closes a cURL session and frees all resources.
-			curl_close( $curl_conn );
-		}
+		// Performs an HTTP request using the POST method.
+		wp_remote_post(
+			$request_url,
+			array(
+				'method' => 'POST',
+				'body'   => $updates,
+			)
+		);
 	}
 }
