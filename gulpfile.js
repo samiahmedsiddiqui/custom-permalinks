b05b320ab25f313d01724798b8e64edb4f67624d
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2023-02-08 15:41:23 +0500

    package updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
 const { src, dest, parallel } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-const del = require('del');
-const package = require("./package.json");
+const package = require('./package.json');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
 // Get Plugin Version from `package.json`
-const pluginVersion = package.customPermalinks.pluginVersion;
+const pluginVersion = package.version;
 
 async function deleteMinFiles() {
-  await del(['assets/**/*.min.*']);
+  const fs = require('fs');
+  const path = require('path');
+
+  fs.readdir('./assets/css/', (err, files) => {
+    files.forEach(file => {
+      if (file.endsWith('.min.css')) {
+        fs.unlinkSync(path.join('./assets/css/', file));
+      }
+    });
+  });
+
+  fs.readdir('./assets/js/', (err, files) => {
+    files.forEach(file => {
+      if (file.endsWith('.min.js')) {
+        fs.unlinkSync(path.join('./assets/js/', file));
+      }
+    });
+  });
 }
 
 function minifyCss() {
   return src(['assets/**/*.css', '!assets/**/*/*.min.css'])
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename(function(path) {
-        path.extname = '-' + pluginVersion + '.min.css';
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(rename(function (path) {
+      path.extname = '-' + pluginVersion + '.min.css';
     }))
     .pipe(dest('./assets'));
 }
@@ -24,8 +40,8 @@ function minifyCss() {
 function minifyJs() {
   return src(['assets/**/*.js', '!assets/**/*/*.min.js'])
     .pipe(uglify())
-    .pipe(rename(function(path) {
-        path.extname = '-' + pluginVersion + '.min.js';
+    .pipe(rename(function (path) {
+      path.extname = '-' + pluginVersion + '.min.js';
     }))
     .pipe(dest('./assets'));
 }
